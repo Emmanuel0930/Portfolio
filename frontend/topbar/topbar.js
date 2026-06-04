fb2be50a57a56241b9574838eb2d9e2413e29ddc
Emmanuel Alvarez Castrillon <ealvarezc1@eafit.edu.co>
2026-06-04 18:13:34 -0500

    Topbar(feat): Designing a topbar for project

---
+++ frontend/topbar/topbar.js
@@ -0,0 +1,47 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'topbar/topbar.css';
+
+document.head.appendChild(link);
+
+fetch('topbar/topbar.html')
+    .then(response => response.text())
+    .then(data => {
+
+        document.getElementById('topbar-container').innerHTML = data;
+
+        startTopbar();
+    });
+
+function startTopbar() {
+    let lastScroll = 0;
+    let upwardDistance = 0;
+
+    const navbar = document.querySelector('.topbar');
+
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.pageYOffset;
+
+        // Bajando
+        if (currentScroll > lastScroll) {
+            navbar.classList.add('hidden');
+            upwardDistance = 0;
+        }
+        // Subiendo
+        else {
+            upwardDistance += lastScroll - currentScroll;
+
+            navbar.classList.remove('hidden');
+
+            if (upwardDistance < 5) {
+                navbar.classList.add('transparent');
+                navbar.classList.remove('visible');
+            } else {
+                navbar.classList.remove('transparent');
+                navbar.classList.add('visible');
+            }
+        }
+
+        lastScroll = currentScroll;
+    });
+}
